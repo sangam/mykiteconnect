b308f0abb2ac1264804ccae7673b5acad908b0e2
sangam <sangamchopade@gmail.com>
2024-12-11 20:19:29 +0530

    Initial Commit

---
+++ mykiteconnect/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals, absolute_import
+
+from mykiteconnect import exceptions
+from mykiteconnect.connect import KiteConnect
+from mykiteconnect.ticker import KiteTicker
+
+__all__ = ["KiteConnect", "KiteTicker", "exceptions"]
---
+++ mykiteconnect/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "mykiteconnect"
+__description__ = "The unofficial Python client for the Kite Connect trading API"
+__url__ = "https://kite.trade"
+__download_url__ = "https://github.com/sangam/mykiteconnect"
+__version__ = "5.0.1"
+__author__ = "Sangam Chopade"
+__license__ = "MIT"
